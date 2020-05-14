6859d26e1834851d8134c2ea231526a83238b3ef
sofieliljedahl <sofli181@student.liu.se>
2020-05-14 10:30:26 +0200

    Updated skybox

--- project/skybox-loader.c
+++ project/skybox-loader.c
@@ -8,30 +8,191 @@
 #include "VectorUtils3.h"
 #include "loadobj.h"
 #include "LoadTGA.h"
+#include <stdio.h>
+#include <string.h>
 
 #include "skybox-loader.h"
 
-GLuint program_sky;
-GLuint *texture;
-Model *model;
+GLuint program, texprogram;
+Model *model[4]; // teapotModel, *sphereModel, *torusModel, *cubeModel;
+Model *box[6];
+int currentModelIndex = 0;
 
-void init_skybox()
+// Globals
+// Data would normally be read from files
+GLfloat vertices[6][6 * 3] =
+	{
+		{
+			// +x
+			0.5, -0.5, -0.5, // 1
+			0.5, 0.5, -0.5,	 // 2
+			0.5, 0.5, 0.5,	 // 6
+			0.5, -0.5, 0.5,	 // 5
+		},
+		{
+			// -x
+			-0.5, -0.5, -0.5, // 0 -0
+			-0.5, -0.5, 0.5,  // 4 -1
+			-0.5, 0.5, 0.5,	  // 7 -2
+			-0.5, 0.5, -0.5,  // 3 -3
+		},
+		{
+			// +y
+			0.5, 0.5, -0.5,	 // 2 -0
+			-0.5, 0.5, -0.5, // 3 -1
+			-0.5, 0.5, 0.5,	 // 7 -2
+			0.5, 0.5, 0.5,	 // 6 -3
+		},
+		{
+			// -y
+			-0.5, -0.5, -0.5, // 0
+			0.5, -0.5, -0.5,  // 1
+			0.5, -0.5, 0.5,	  // 5
+			-0.5, -0.5, 0.5	  // 4
+		},
+		{
+			// +z
+			-0.5, -0.5, 0.5, // 4
+			0.5, -0.5, 0.5,	 // 5
+			0.5, 0.5, 0.5,	 // 6
+			-0.5, 0.5, 0.5,	 // 7
+		},
+		{
+			// -z
+			-0.5, -0.5, -0.5, // 0
+			-0.5, 0.5, -0.5,  // 3
+			0.5, 0.5, -0.5,	  // 2
+			0.5, -0.5, -0.5,  // 1
+		}};
+
+GLfloat texcoord[6][6 * 2] =
+	{
+		{
+			1.0,
+			1.0,
+			1.0,
+			0.0, // left OK
+			0.0,
+			0.0,
+			0.0,
+			1.0,
+		},
+		{
+			0.0,
+			1.0, // right OK
+			1.0,
+			1.0,
+			1.0,
+			0.0,
+			0.0,
+			0.0,
+		},
+		{
+			1.0,
+			0.0, // top OK
+			0.0,
+			0.0,
+			0.0,
+			1.0,
+			1.0,
+			1.0,
+		},
+		{
+			0.0,
+			1.0,
+			1.0,
+			1.0,
+			1.0,
+			0.0, // bottom
+			0.0,
+			0.0,
+		},
+		{
+			0.0,
+			1.0,
+			1.0,
+			1.0,
+			1.0,
+			0.0, // back OK
+			0.0,
+			0.0,
+		},
+		{
+			1.0,
+			1.0,
+			1.0,
+			0.0, // front OK
+			0.0,
+			0.0,
+			0.0,
+			1.0,
+		}};
+GLuint indices[6][6] =
+	{
+		{0, 2, 1, 0, 3, 2},
+		{0, 2, 1, 0, 3, 2},
+		{0, 2, 1, 0, 3, 2},
+		{0, 2, 1, 0, 3, 2},
+		{0, 2, 1, 0, 3, 2},
+		{0, 2, 1, 0, 3, 2}};
+
+#define TEXTURE_OFFSET 0
+
+char *textureFileName[6] =
+	{
+		"resources/skybox/textures/px.tga", // ok
+		"resources/skybox/textures/nx.tga", // ok
+		"resources/skybox/textures/py.tga", // ok
+		"resources/skybox/textures/ny.tga", // ok
+		"resources/skybox/textures/pz.tga", // ok
+		"resources/skybox/textures/nz.tga", // ok
+};
+
+TextureData t[6];
+void loadTextures()
 {
-	model = LoadModelPlus("resources/skybox/skybox.obj");
-	program_sky = loadShaders("shaders/skybox.vert", "shaders/skybox.frag");
-	glUseProgram(program_sky);
-	glActiveTexture(GL_TEXTURE1);
-	glUniform1i(glGetUniformLocation(program_sky, "skyTex"), 1);
-	LoadTGATextureSimple("resources/skybox/skybox.tga", &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);		// Bind Our Texture tex1	
+	int i;
+	glActiveTexture(GL_TEXTURE1); // Just make sure the texture unit match
+	// Load texture data and create ordinary texture objects (for skybox)
+	for (i = 0; i < 6; i++)
+	{
+		printf("Loading texture %s\n", textureFileName[i + TEXTURE_OFFSET]);
+		LoadTGATexture(textureFileName[i + TEXTURE_OFFSET], &t[i]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	}
 }
 
-void draw_skybox(mat4 projectionMatrix, mat4 cameraMatrix, mat4 transformMatrix)
+void skybox_init(mat4 projMatrix)
 {
-	glUseProgram(program_sky);
-	glUniformMatrix4fv(glGetUniformLocation(program_sky, "projectionMatrix"), 1, GL_TRUE, projectionMatrix.m);
-	glUniformMatrix4fv(glGetUniformLocation(program_sky, "transform"), 1, GL_TRUE, transformMatrix.m);
-	glBindTexture(GL_TEXTURE_2D, texture); //sky texture
-	glUniformMatrix4fv(glGetUniformLocation(program_sky, "view"), 1, GL_TRUE, cameraMatrix.m);
-	DrawModel(model, program_sky, "inPosition", "inNormal", "inTexCoord");
+	int i;
+	for (i = 0; i < 6; i++)
+	{
+		box[i] = LoadDataToModel(vertices[i], NULL, texcoord[i], NULL, indices[i], 4, 6);
+	}
+	texprogram = loadShaders("shaders/tex.vert", "shaders/tex.frag");
+	glUniformMatrix4fv(glGetUniformLocation(texprogram, "projMatrix"), 1, GL_TRUE, projMatrix.m);
+	glUniform1i(glGetUniformLocation(texprogram, "tex"), 1); // unit 1
+	glEnable(GL_DEPTH_TEST);
+	loadTextures();
+}
+
+void skybox_display(mat4 cameraMatrix)
+{
+	int i;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glDisable(GL_DEPTH_TEST);
+	glUseProgram(texprogram);
+	cameraMatrix.m[3] = 0;
+	cameraMatrix.m[7] = 0;
+	cameraMatrix.m[11] = 0;
+	glUniformMatrix4fv(glGetUniformLocation(texprogram, "worldToViewMatrix"), 1, GL_TRUE, cameraMatrix.m);
+	for (i = 0; i < 6; i++)
+	{
+		glBindTexture(GL_TEXTURE_2D, t[i].texID);
+		DrawModel(box[i], texprogram, "inPosition", NULL, "inTexCoord");
+	}
+	glEnable(GL_DEPTH_TEST);
+	glActiveTexture(GL_TEXTURE1);
+	glutSwapBuffers();
 }
